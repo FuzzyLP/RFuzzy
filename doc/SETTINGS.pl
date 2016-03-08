d20df3a50a80631b105fb7e54ccb67b1560d43dd
Jose F. Morales <jfmcjf@gmail.com>
2016-03-08 15:10:37 +0100

    Simplified lpdoc manual (needs latest ciao version)

--- doc/SETTINGS.pl
+++ doc/SETTINGS.pl
@@ -2,16 +2,7 @@
 
 :- doc(title, "Configuration for RFuzzy manual").
 
-datamode(_) :- fail.
-execmode(_) :- fail.
-
 bibfile := ~fsR(bundle_src('RFuzzy')/doc/'bibliography.bib').
-
-% (not customized)
-htmldir(_) :- fail.
-docdir(_) :- fail.
-infodir(_) :- fail.
-mandir(_) :- fail.
 
 filepath := ~fsR(bundle_src('RFuzzy')/doc).
 filepath := ~fsR(bundle_src('RFuzzy')/lib).
@@ -20,18 +11,3 @@ filepath := ~fsR(bundle_src('RFuzzy')). % (for examples/)
 output_name := 'RFuzzy'.
 
 doc_structure := 'rfuzzy/rfuzzy_doc'.
- 
-commonopts := no_bugs|no_patches.
-doc_mainopts := ~commonopts.
-doc_compopts := ~commonopts.
-
-index := concept|lib|pred|prop|regtype|decl|author|global.
-
-startpage := 1.
-
-papertype := afourpaper.
-
-libtexinfo := 'yes'.
-
-docformat := texi|ps|pdf|manl|info|html.
-
